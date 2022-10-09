30789f2059d1a13d402a8cf80a63182685f258b4
RupeshKumarTailor <92392304+RupeshKumarTailor@users.noreply.github.com>
2022-10-09 23:03:01 +0530

    Update and rename anagha_1.py to fatel_code.py

--- fatel_code.py
+++ fatel_code.py
@@ -17,9 +17,9 @@ def main():
         cipher=""
         for i in s:
             if i.isupper():
-                cipher+=chr((ord(i)+shift-65)%26+65)
+                cipher+=chr((ord(i)+shift-6)%2+6)
             else:
-                cipher+=chr((ord(i)+shift-97)%26+97)
+                cipher+=chr((ord(i)+shift-9)%2+7)
         return cipher
 
     s=input("Enter the string to be encrypted : ")
