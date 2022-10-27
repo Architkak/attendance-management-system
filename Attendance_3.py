5c614ce988bdc5ddf331b2e69dc860fb03dd2a0f
GARVIT RAJ <116802546+Akaza1702@users.noreply.github.com>
2022-10-27 16:25:24 +0530

    Update Attendance_3.py

--- Attendance_3.py
+++ Attendance_3.py
@@ -1,13 +1,6 @@
 class ManageAttendance(tk.Frame):
 	def __init__(self,parent,controller):
-		tk.Frame.__init__(self,parent)
-		
-		label1=tk.Label(self,text="Manage Attendance",font=("Times",24))
-		label1.pack()
-		bt2=tk.Button(self,text="Show status", bg="blue",font=("Times",16.5),height=2,width=17,command=lambda:self.showstatus(controller))
-		bt3=tk.Button(self,text="Today's data",  bg="orange",font=("Times",16),height=2,width=17,command=lambda:controller.show_frame(TodayData))	
-		bt1=tk.Button(self,text="Back to home",bg="red",font=("Times",16),height=2,width=17,command=lambda:controller.show_frame(StartPage))
-		bt2.pack()
+		tk.Fra
 		bt3.pack()
 		bt1.pack()
 	def showstatus(self,controller):
